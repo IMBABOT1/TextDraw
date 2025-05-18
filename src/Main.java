be5391dd3c168e8fdf03f4993150c4e27d3c546e
unknown <zlovred12@gmail.com>
2025-05-18 18:22:38 +0300

    temp

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args){
+        method1();
+        method2();
+        method3();
+    }
+
+    private static void method1(){
+        System.out.println("Hello, world");
+        System.out.println("This is Java!");
+    }
+
+    private static void method2(){
+        System.out.println("Hello, world");
+        System.out.println("This is Java!");
+    }
+
+    private static void method3(){
+        System.out.println("===========");
+        System.out.println("= Привет! =");
+        System.out.println("===========");
+    }
+}
