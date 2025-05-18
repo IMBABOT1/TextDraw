4a6fc70dc94e697fe4ad4d9a9144ec9b5923978d
unknown <zlovred12@gmail.com>
2025-05-18 18:25:59 +0300

    temp

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,9 @@ public class Main {
     }
 
     private static void method2(){
-        System.out.println("Hello, world");
-        System.out.println("This is Java!");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("*******");
     }
 
     private static void method3(){
